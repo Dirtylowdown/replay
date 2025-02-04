9fb971f630404d413f8c9d60e92d5353610a3148
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-03 18:04:16 -0600

    Update LighthouseStringifyExtension.test.ts.js

--- __snapshots__/LighthouseStringifyExtension.test.ts.js
+++ __snapshots__/LighthouseStringifyExtension.test.ts.js
@@ -1,105 +1,43 @@
-exports['LighthouseStringifyExtension handles ending timespan 1'] = `
-const fs = require('fs');
-const puppeteer = require('puppeteer'); // v23.0.0 or later
+end
+delete
+stop 
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const timeout = 5000;
-  page.setDefaultTimeout(timeout);
 
-  const lhApi = await import('lighthouse'); // v10.0.0 or later
-  const flags = {
-    screenEmulation: {
-      disabled: true
-    }
-  }
-  const config = undefined;
-  const lhFlow = await lhApi.startFlow(page, {name: 'Test Flow', config, flags});
-  {
-    const targetPage = page;
-    await targetPage.setViewport({
-      width: 757,
-      height: 988
-    })
-  }
-  await lhFlow.startNavigation();
-  {
-    const targetPage = page;
-    await targetPage.goto('https://example.com');
-  }
-  await lhFlow.endNavigation();
-  await lhFlow.startTimespan();
-  {
-    const targetPage = page;
-    await puppeteer.Locator.race([
-      targetPage.locator('#button')
-    ])
-      .setTimeout(timeout)
-      .click({
-        offset: {
-          x: 61,
-          y: 13.5625,
-        },
-      });
-  }
-  await lhFlow.endTimespan();
-  const lhFlowReport = await lhFlow.generateReport();
-  fs.writeFileSync(__dirname + '/flow.report.html', lhFlowReport)
 
-  await 
-`;
 
-exports['LighthouseStringifyExtension handles ending navigation 1'] = `
-const fs = require('fs');
-const puppeteer = require('puppeteer'); // v23.0.0 or later
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const timeout = 5000;
-  page.setDefaultTimeout(timeout);
 
-  const lhApi = await import('lighthouse'); // v10.0.0 or later
-  const flags = {
-    screenEmulation: {
-      disabled: true
-    }
-  }
-  const config = undefined;
-  const lhFlow = await lhApi.startFlow(page, {name: 'Test Flow', config, flags});
-  {
-    const targetPage = page;
-    await targetPage.setViewport({
-      width: 757,
-      height: 988
-    })
-  }
-  await lhFlow.startNavigation();
-  {
-    const targetPage = page;
-    await targetPage.goto('https://example.com');
-  }
-  await lhFlow.endNavigation();
-  await lhFlow.startTimespan();
-  {
-    const targetPage = page;
-    await puppeteer.Locator.race([
-      targetPage.locator('#button')
-    ])
-      .setTimeout(timeout)
-      .click({
-        offset: {
-          x: 61,
-          y: 13.5625,
-        },
-      });
-  }
-  await lhFlow.endTimespan();
-  await lhFlow.startNavigation();
-  {
-    const targetPage = page;
-    await targetPage.goto('https://example.com/page/');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+targetPage.goto('https://example.com/page/');
   }
   await lhFlow.endNavigation();
   const lhFlowReport = await lhFlow.generateReport();
